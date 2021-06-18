a4009a30fcd94975a4573c08e2cce7c428adede7
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-18 14:22:49 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -97,20 +97,59 @@ const icons = [
     },
 ];
 
-stampa (icons)
 
+// colorazione oggetti
+const iconsColor = icons.map((obj) => {
+    let color;
+    if (obj.type == 'animal') {
+        color = 'brown';
+    } else if (obj.type == 'vegetable') {
+        color = 'green';
+    } else {
+        color = 'violet';
+    } 
+    return {
+        ...obj, color
+    }
+});
+
+stampa (iconsColor)
+
+// inserimento valori in select
+const tipi = [];
+iconsColor.forEach((obj) => {
+    if (!tipi.includes(obj.type)){
+        tipi.push(obj.type);
+        document.getElementById('tipo').innerHTML += `<option value="${obj.type}">${obj.type}</option>`
+    }
+});
+
+
+// cambio nel select
+const selezione = document.getElementById('tipo');
+selezione.addEventListener("change", function(){
+    const filtrato = iconsColor.filter((val)=>{
+        return val.type == selezione.value ;
+    });
+    stampa(filtrato);
+    if(selezione.value == 'all'){
+        stampa(iconsColor);
+    };
+});
 
 // funzioni
 function stampa (array) {
+    document.getElementById('icons').innerHTML = '';
     array.forEach(obj => {
         document.getElementById('icons').innerHTML += `
         <div class="col g-3">
             <div class="box-obj text-center ">
-                <i class="${obj.family} ${obj.prefix}${obj.name}"></i>
+                <i style="color: ${obj.color}" class="${obj.family} ${obj.prefix}${obj.name}"></i>
                 <h5 class="upper">${obj.name}</h5>
             </div>
         </div>`
     });
 
 };
+
 
